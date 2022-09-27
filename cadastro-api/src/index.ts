b2b2ebe7816f238f2f04f4bdbc348f453da97c0b
Varela <Varela>
2022-09-27 10:00:07 -0300

    ...

--- cadastro-api/src/index.ts
+++ cadastro-api/src/index.ts
@@ -14,6 +14,7 @@ app.use("/", express.static("../cadastro/dist"))
 // BUSCAR TODOS OS USUÁRIOS
 app.get("/api/users", (req, res) => {
    const sql = "SELECT id, name, email FROM user"
+
    database.all(sql, [], (err, rows) => {
       if (err) {
          res.status(400).json({ "error": err.message })
@@ -30,6 +31,7 @@ app.get("/api/users", (req, res) => {
 // BUSCAR UM USUÁRIO
 app.get("/api/user/:id", (req, res) => {
    const sql = "SELECT id, name, email FROM user WHERE id = ?"
+
    database.get(sql, [req.params.id], (err, row) => {
       if (err) {
          res.status(400).json({ "error": err.message });
@@ -118,5 +120,40 @@ app.delete("/api/user/:id", (req, res) => {
       }
    )
 })
+
+app.post("/api/login/", (req, res) => {
+   const sql = "SELECT id, name, email FROM user WHERE email=? AND password=?"
+   const { email, password } = req.body
+
+   database.get(sql, [email, password], (err, row) => {
+      if (err) {
+         res.status(400).json({ "error": err.message });
+         return;
+      }
+
+      res.json({
+         "message": "success",
+         "data": row ?? "none"
+      })
+   })
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 app.listen(port, () => console.log(`⚡ servidor ${port}`))
