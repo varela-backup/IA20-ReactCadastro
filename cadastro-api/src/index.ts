f6602e718e508f91095ca156a0e6d65dd544d64e
Varela <Varela>
2022-09-27 08:53:43 -0300

    adicionado rota get user e users

--- cadastro-api/src/index.ts
+++ cadastro-api/src/index.ts
@@ -1,12 +1,39 @@
 import express from "express"
+import database from "./database"
 
 const port = 8080
 const app = express()
 
 app.use("/", express.static("../cadastro/dist"))
 
-app.get("/api", (req, res) => {
-   res.json({ resposta: "oi tudo bem?" })
+app.get("/api/users", (req, res) => {
+   const sql = "SELECT id, name, email FROM user"
+   database.all(sql, [], (err, rows) => {
+      if (err) {
+         res.status(400).json({ "error": err.message })
+         return
+      }
+
+      res.json({
+         "message": "success",
+         "data": rows
+      })
+   })
 })
 
-app.listen(port, () => console.log(`⚡ servidor rodando na porta ${port}`))
+app.get("/api/user/:id", (req, res) => {
+   const sql = "SELECT id, name, email FROM user WHERE id = ?"
+   database.get(sql, [req.params.id], (err, row) => {
+      if (err) {
+         res.status(400).json({ "error": err.message });
+         return;
+      }
+
+      res.json({
+         "message": "success",
+         "data": row
+      })
+   })
+})
+
+app.listen(port, () => console.log(`⚡ servidor ${port}`))
