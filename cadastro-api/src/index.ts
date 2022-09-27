7fbba02d9dc0d97950f7030c49d7a4ceb0a2f007
Varela <Varela>
2022-09-27 09:45:21 -0300

    adicionado rotas user PATCH e POST

--- cadastro-api/src/index.ts
+++ cadastro-api/src/index.ts
@@ -1,11 +1,17 @@
 import express from "express"
+import { RunResult } from "sqlite3"
 import database from "./database"
 
 const port = 8080
 const app = express()
 
+// 
+app.use(express.json())
+
+// STATIC
 app.use("/", express.static("../cadastro/dist"))
 
+// BUSCAR TODOS OS USUÁRIOS
 app.get("/api/users", (req, res) => {
    const sql = "SELECT id, name, email FROM user"
    database.all(sql, [], (err, rows) => {
@@ -21,6 +27,7 @@ app.get("/api/users", (req, res) => {
    })
 })
 
+// BUSCAR UM USUÁRIO
 app.get("/api/user/:id", (req, res) => {
    const sql = "SELECT id, name, email FROM user WHERE id = ?"
    database.get(sql, [req.params.id], (err, row) => {
@@ -34,6 +41,67 @@ app.get("/api/user/:id", (req, res) => {
          "data": row
       })
    })
+})
+
+// CREATE
+app.post("/api/user/", (req, res) => {
+   const errors = []
+
+   if (!req.body.password)
+      errors.push("No password specified")
+
+   if (!req.body.email)
+      errors.push("No email specified")
+
+   if (errors.length) {
+      res.status(400).json({ "error": errors.join() })
+      return;
+   }
+
+   const { name, email, password } = req.body
+   const sql = 'INSERT INTO user (name, email, password) VALUES (?,?,?)'
+   const params = [name, email, password]
+
+   database.run(sql, params, function (this: RunResult, err) {
+      if (err) {
+         res.status(400).json({ "error": err.message })
+         return
+      }
+
+      res.json({
+         "message": "success",
+         "data": { name, email, password },
+         "id": this.lastID
+      })
+   })
+})
+
+// UPDATE
+app.patch("/api/user/:id", (req, res) => {
+   const { name, email, password } = req.body
+   const sql = `
+      UPDATE user SET 
+         name = COALESCE(?,name), 
+         email = COALESCE(?,email), 
+         password = COALESCE(?,password) 
+      WHERE 
+         id = ?
+   `
+
+   database.run(sql, [name, email, password, req.params.id],
+      function (this: RunResult, err) {
+         if (err) {
+            res.status(400).json({ "error": err.message })
+            return;
+         }
+
+         res.json({
+            message: "success",
+            data: { name, email, password },
+            changes: this.changes
+         })
+      }
+   )
 })
 
 app.listen(port, () => console.log(`⚡ servidor ${port}`))
